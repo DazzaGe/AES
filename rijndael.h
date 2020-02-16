33d518fd0657ac3ff470a4c6563c61f69e26e2d6
DazzaGe <darren@d0ntspam.me>
2020-02-16 23:31:36 +1300

    Created Inverse cipher and corresponding inverse functions

--- rijndael.h
+++ rijndael.h
@@ -12,5 +12,5 @@
 
 
 void    Rijndael_Cipher(uint8_t state[Nb][4], uint32_t* keySchedule, unsigned int Nr);
-void    Rijndael_InvCipher();
+void    Rijndael_InvCipher(uint8_t state[Nb][4], uint32_t* keySchedule, unsigned int Nr);
 void    Rijndael_KeyExpansion(uint32_t* key, uint32_t* keySchedule, unsigned int Nk, unsigned int Nr);
